bd104e72ce9e5c1fa412b36ade518b67f8193e26
Matias <queiroloco@gmail.com>
2019-06-11 16:37:09 -0400

    first commit

---
+++ lang/en/local_ejemplo.php
@@ -0,0 +1,14 @@
+
+<?php
+/**
+ * Strings for component 'local_apuntes', language 'en'
+ *
+ * @package   local_apuntes
+ */
+ 
+ /**
+local/apuntes.php
+*/
+$string["title"] = "Class Notes UAI";
+$string["heading"] = "Class Notes UAI";
+$string['pluginname']="Ejemploooooo";
---
+++ version.php
@@ -0,0 +1,33 @@
+
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *
+*
+* @package    local
+* @subpackage apuntes
+* @copyright  2016 Matías Queirolo (mqueirolo@alumnos.uai.cl) 					
+* @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+//Pertenece al plugin Apuntes
+
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->pluginname = "local_ejemplo";
+$plugin->component = 'local_ejemplo';
+$plugin->version   = '2019061103';
+$plugin->requires = 2015111600;
